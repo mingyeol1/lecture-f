72474f0c38fdad9de633b63ad428d8938696eb0f
Mingyeol <alsruf741@gmail.com>
2024-12-03 17:19:08 +0900

    백엔드랑 연동 완료

---
+++ src/Test.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { axiosInstance } from "./config"; // 위에서 만든 axiosInstance를 import
+
+const Test = () => {
+    const [message, setMessage] = useState<string>(""); // API 응답을 저장할 상태
+
+    useEffect(() => {
+        // API 호출
+        axiosInstance
+            .get("test") // GET 요청
+            .then((response) => {
+                // 성공 시
+                setMessage(response.data); // 응답 데이터 설정
+            })
+            .catch((error) => {
+                // 실패 시
+                console.error("Error fetching test API:", error);
+                setMessage("Failed to fetch data");
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Test API Response</h1>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default Test;
---
+++ src/config.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+const API_URL = 'http://localhost:8080/api/';
+
+// Axios 인스턴스 생성
+export const axiosInstance = axios.create({
+    withCredentials: true,
+    baseURL: API_URL,
+    headers: { 'Content-Type': 'application/json' },
+});
+
